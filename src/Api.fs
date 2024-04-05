787a108d0120558b38ba33325bea3c4b8d29ff52
SimonNyvall <simon.nyvall@proton.me>
2024-04-05 16:04:21 +0200

    work in progress: .

---
+++ src/Api.fs
@@ -0,0 +1,41 @@
+﻿module Vortex.Api
+
+open System
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Http
+
+type RequestResponse = HttpContext -> obj
+
+let mapGet (pattern: string) (handler: RequestResponse) (app: WebApplication) =
+    app.MapGet(pattern, Func<HttpContext, obj> handler) |> ignore
+
+    app
+
+let mapPost (pattern: string) (handler: RequestResponse) (app: WebApplication) =
+    app.MapPost(pattern, Func<HttpContext, obj> handler) |> ignore
+
+    app
+
+let mapPut (pattern: string) (handler: RequestResponse) (app: WebApplication) =
+    app.MapPut(pattern, Func<HttpContext, obj> handler) |> ignore
+
+    app
+
+let mapDelete (pattern: string) (handler: RequestResponse) (app: WebApplication) =
+    app.MapDelete(pattern, Func<HttpContext, obj> handler) |> ignore
+
+    app
+
+// TODO add a type that works like HttpContext -> obj
+// TODO allow for async functions
+// TODO make a better folder strucutre
+// TODO add a way to get the body of a Request
+// TODO add a way to get the headers of a Request
+// TODO add a way to get the cookies of a Request
+// TODO add a way to get the session of a Request
+// TODO add a way to get the user of a Request
+// TODO add a way to get the user claims of a Request
+// TODO add a way to get the user roles of a Request
+// TODO add a way to get the user permissions of a Request
+// TODO add a way to get the user policies of a Request
+// TODO add json serialization for the api
---
+++ tests/Program.fs
@@ -0,0 +1,33 @@
+﻿namespace Vortex.Tests
+
+open System
+open Vortex.Api
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Http
+
+
+module Tests =
+
+
+    [<EntryPoint>]
+    let main argv =
+        let builder = WebApplication.CreateBuilder(argv)
+
+        let app = builder.Build()
+
+        let handler context : HttpContext -> obj = Results.Ok("Hello world")
+
+        // get simple hello world test
+        app |> mapGet "/test" handler |> ignore
+
+        // get query test
+        app
+        |> mapGet "/query" (fun context ->
+            let id = context |> query "id"
+
+            Results.Ok(id))
+        |> ignore
+
+        app.Run()
+
+        0
